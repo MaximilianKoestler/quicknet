3ffb375c6c20d62effabfccbeb3671eb2b0a0667
Maximilian Köstler <maximilian.koestler@tuhh.de>
2017-03-22 14:22:39 +0100

    Model to CPP converter

---
+++ tools/modeltocpp.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+import json
+
+from argparse import ArgumentParser
+
+def main(args):
+    with open(args.input) as f:
+        network = json.load(f)
+
+    indent = ' ' * 4
+ 
+    code_arrays = []
+    code_layers = ''
+ 
+    num_layers = len(network['layers'])
+ 
+    code_layers += 'static quicknet::Layer layers[%d] {\n' % (num_layers)
+ 
+    for i, layer in enumerate(network['layers']):
+        code_arrays.append('/******** Layer %d ********/' % i)
+
+        code_arrays.append('static const float l%d_weights_array[%d] {' % (i, layer['inputs'] * layer['outputs']))
+        for row in layer['weights']:
+            code_arrays.append(indent + ','.join(['%.8f' % v for v in row]) + ',')
+        code_arrays.append('};')
+ 
+        code_arrays.append('static float l%d_bias_array[%d] {' % (i, layer['outputs']))
+        code_arrays.append(indent + ','.join(['%.8f' % v for v in layer['bias']]))
+        code_arrays.append('};')
+ 
+        code_arrays.append('static float l%d_output_array[%d];' % (i, layer['outputs']))
+        code_arrays.append('')
+
+        code_arrays.append('static const quicknet::matrix_t l%d_weights{%d,%d,l%d_weights_array};' % (i, layer['outputs'], layer['inputs'], i))
+        code_arrays.append('static const quicknet::vector_t l%d_bias{%d,l%d_bias_array};' % (i, layer['outputs'], i))
+        code_arrays.append('static quicknet::vector_t l%d_output{%d,l%d_output_array};' % (i, layer['outputs'], i))
+        code_arrays.append('')
+ 
+        code_layers += indent + '{l%d_weights, l%d_bias, l%d_output, quicknet::quick_%s},\n' % (i, i, i, layer['activation'])
+ 
+    code_layers += '};\n\n'
+ 
+    code_cpp = '#include "NeuralNetwork.h"\n\n'
+    code_cpp += '\n'.join(code_arrays) + '\n'
+    code_cpp += '/******** Network ********/\n'
+    code_cpp += code_layers
+    code_cpp += 'NeuralNetwork::NeuralNetwork() : network{%d, layers} {\n}\n' % num_layers
+ 
+    with open(args.output, 'w') as f:
+        f.write(code_cpp)
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+
+    parser.add_argument('input', type=str, help='Input file')
+    parser.add_argument('output', type=str, help='Output file')
+
+    main(parser.parse_args())
+
